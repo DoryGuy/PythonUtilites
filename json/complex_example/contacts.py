0a8845c1118068ce4aad5cf6eacb8d1aa7161606
DoryGuy <gwpowell@hotmail.com>
2025-08-10 15:33:53 -0700

    use json_decorators

--- json/complex_example/contacts.py
+++ json/complex_example/contacts.py
@@ -5,7 +5,8 @@ A class to hold a contact. (demo)
 
 
 from dataclasses import dataclass
-import json
+from json_decorators import json_decortor
+from json_encoders_decoders import MyJsonEncoder, MyJsonDecoder
 from json_register import json_class_registry
 
 @dataclass(kw_only=True)
@@ -14,44 +15,15 @@ class Contact:
 
 @json_class_registry.register
 @dataclass(kw_only=True)
+@json_decorator(encoder=MyJsonEncoder, decoder=MyJsonDecoder)
 class Email(Contact):
     ''' Email class '''
     address: str = ""
 
-    def to_json(self) -> str:
-        """ dump to json """
-        return json.dumps(self.__dict__,sort_keys=True,indent=4)
-
-    @classmethod
-    def from_json(cls, json_stuff):
-        """ create a Phone Contact from json """
-        # pylint: disable=possibly-used-before-assignment
-        if isinstance(json_stuff, (bytes, bytearray, str)):
-            data = json.loads(json_stuff)
-        elif isinstance(json_stuff, dict):
-            data = json_stuff
-        return cls(**data)
-
-
-
 @json_class_registry.register
 @dataclass(kw_only=True)
+@json_decorator(encoder=MyJsonEncoder, decoder=MyJsonDecoder)
 class Phone(Contact):
     ''' Phone class '''
     type: str = ""
     number: str = ""
-
-
-    def to_json(self) -> str:
-        """ dump to json """
-        return json.dumps(self.__dict__,sort_keys=True,indent=4)
-
-    @classmethod
-    def from_json(cls, json_stuff):
-        """ create a Phone Contact from json """
-        # pylint: disable=possibly-used-before-assignment
-        if isinstance(json_stuff, (bytes, bytearray, str)):
-            data = json.loads(json_stuff)
-        elif isinstance(json_stuff, dict):
-            data = json_stuff
-        return cls(**data)

7e6f86b01525103061584a82857645cd5e290bb1
DoryGuy <gwpowell@hotmail.com>
2025-08-10 15:41:35 -0700

    use the decorators in the management class.

--- json/json_decorators/example/management.py
+++ json/json_decorators/example/management.py
@@ -1,1 +1,37 @@
-../../basic/example/management.py
+#
+# pylint: disable=line-too-long
+#
+"""
+# Exercise No.   Hw12 Project 1
+# File Name:     hw10GetPositiveInteger.py
+# Programmer:    Gary Powell
+# Date:          April 14, 2025
+#
+# Problem Statement: Classes to hold a management tool.
+#
+# Overall Plan:
+"""
+
+import json
+from typing import Dict, Optional
+
+from employee import Employee
+from json_decorators import json_decorator
+from json_encoders_decoders import MyJsonEncoder, MyJsonDecoder
+
+@json_decorators(decode=MyJsonDecoder)
+class EmployeeManagement:
+    """ Hold all the employees """
+    def __init__(self, *,next_employee_id:int = 1, employees: Optional[Dict[str, Employee]] = None) -> None: #
+        """ initialize the class """
+        if employees is None:
+            employees = {}
+        self.next_employee_id = next_employee_id
+        self.employees: Dict[str, Employee] = employees
+
+    def to_json(self) -> str:
+        """ dump to json """
+        return json.dumps(self.__dict__,
+                          sort_keys = True,
+                          indent=4,
+                          cls=MyJsonEncoder)

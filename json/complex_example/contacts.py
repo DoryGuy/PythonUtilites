af2e0ce0afd4a05c33eb784f11997aa814d78626
DoryGuy <gwpowell@hotmail.com>
2025-08-13 17:17:22 -0700

    bug fixes.

--- json/complex_example/contacts.py
+++ json/complex_example/contacts.py
@@ -15,14 +15,14 @@ class Contact:
 
 @json_class_registry.register
 @dataclass(kw_only=True)
-@json_decorator(encoder=MyJsonEncoder, decoder=MyJsonDecoder)
+@json_decorator(encoder=MyJsonEncoder, decoder=MyJsonDecoder, indent=4, sort_keys=True)
 class Email(Contact):
     ''' Email class '''
     address: str = ""
 
 @json_class_registry.register
 @dataclass(kw_only=True)
-@json_decorator(encoder=MyJsonEncoder, decoder=MyJsonDecoder)
+@json_decorator(encoder=MyJsonEncoder, decoder=MyJsonDecoder, indent=4, sort_keys=True)
 class Phone(Contact):
     ''' Phone class '''
     type: str = ""
